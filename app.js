bc63b9bb0252f91a66026610ac4db0545c4ffa0b
Xil Pham <you@example.com>
2022-09-23 10:23:14 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
+
 
 const app = express();
 
@@ -11,5 +13,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
